695dd1d783f578a66756224cdc58067d4c2ef637
TANT-A01\newbh01 <newbh01@ca.com>
2017-07-07 01:21:05 +0530

    first commit - basic skeleton of electron and ng2

---
+++ README.md
@@ -0,0 +1,2 @@
+# ng2-electron-material
+Using Angular2 with Electron & material
---
+++ app/app.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-app',
+  template: '<h1>Clara Mobile</h1>'
+})
+export class AppComponent { }
