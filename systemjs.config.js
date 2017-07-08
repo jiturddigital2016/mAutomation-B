f6eeea73cff16aea6f03e106954d8c5ef14c24b0
rddigital2016@gmail.com <rddigital2016@gmail.com>
2017-07-08 22:43:03 +0530

    material implementation

--- systemjs.config.js
+++ systemjs.config.js
@@ -24,9 +24,14 @@
       '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
       '@angular/upgrade': 'npm:@angular/upgrade/bundles/upgrade.umd.js',
       '@angular/upgrade/static': 'npm:@angular/upgrade/bundles/upgrade-static.umd.js',
+
       // other libraries
       'rxjs':                      'npm:rxjs',
-      'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js'
+      'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js',
+      '@angular2-material/core': 'npm:@angular2-material/core/core.umd.js',
+      '@angular2-material/card': 'npm:@angular2-material/card/card.umd.js',
+      '@angular2-material/button': 'npm:@angular2-material/button/button.umd.js',
+      '@angular2-material/icon': 'npm:@angular2-material/icon/icon.umd.js'
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
