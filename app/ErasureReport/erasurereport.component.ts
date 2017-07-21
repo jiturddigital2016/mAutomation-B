7e2a97da1bad662fa1bf946bae28f122718a47ab
TANT-A01\newbh01 <newbh01@ca.com>
2017-07-21 20:39:22 +0530

    List view of usb device connected!

--- app/ErasureReport/erasurereport.component.ts
+++ app/ErasureReport/erasurereport.component.ts
@@ -14,11 +14,11 @@ import { DataShare } from '../DataShare/datashare';
 export class ErasurereportComponent{
 	
 	private url:string='http://localhost:3000/devicesList';
-	
+	public devices = [];
 	constructor(private http: Http,private webservice: WebServiceComponent,private router: Router, private datashare:DataShare) {
-
-		this.webservice.devicesList(this.url).subscribe(data =>{
-			console.log(data);
+		this.webservice.devicesList(this.url)
+		.subscribe(data => {  
+			this.devices = data.json();
 		});
 	}
 
