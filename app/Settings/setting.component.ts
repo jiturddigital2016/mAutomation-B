22d8111826856bddaeb7034c6a4af6fcae2366ec
VInoth <vinoth@Apples-Mac-mini-2.local>
2017-07-25 22:01:15 +0530

    Designs Alignment

--- app/Settings/setting.component.ts
+++ app/Settings/setting.component.ts
@@ -29,10 +29,33 @@ Site:Array<any>;
 compare:String;
 userid:string;
 usertype:string;
+deviceimages_List:Array<any>
+
 
 public edited = false;
 
 site:boolean=true;
+
+
+
+
+
+
+
+tabs = [
+  {name: 'TEST SUITES'},
+  {name: 'DEVICE IMAGES'},
+  {name: 'CLIENTS'},
+   {name: 'TECHNICIANS'},
+   { name: 'SITES'},
+  { name: 'ABOUT'}
+
+
+];
+
+
+
+
 
 
 constructor(private http: Http,public dialog: MdDialog,private datashare:DataShare,private webservice: WebServiceComponent) {
@@ -42,6 +65,15 @@ constructor(private http: Http,public dialog: MdDialog,private datashare:DataSha
     { name: 'All Automated Tests' }
     
     ];
+
+
+
+
+
+
+
+
+
 
 this.usertype=this.datashare.logindetails[0].user_type;
 
@@ -64,6 +96,11 @@ else
 
 ngOnInit()
 {
+
+
+
+
+
  this.http.get(this.datashare.Display_Clientdetails_API)
       .subscribe(data => {
         console.log(data.json().Client); 
@@ -93,7 +130,7 @@ this.Testsuites_webservice_call();
 
 
 
-
+this.Deviceimages_webservice_call();
 
 
 
@@ -101,6 +138,10 @@ this.Testsuites_webservice_call();
 
 
 }
+
+
+
+
 
 Testsuites_webservice_call()
 {
@@ -112,13 +153,13 @@ if(this.usertype == "admin")
 {
   
 data1.append('adminid',this.userid); 
-  console.log("**************"); 
+
 }
 else
 {
   
 data1.append('technicianid',this.userid);
-  console.log("$$$$$$$$$$$$$"); 
+ 
 }
 
 
@@ -131,6 +172,49 @@ console.log(body);
  console.log(data.json());
 this.datashare.edit_testsuites=data.json().status;
 this.datashare.Edit_TestSuites_Detials=data.json().testsuitedata;
+
+  err =>
+  {
+alert("error getting")
+
+  }
+
+});
+
+
+
+
+}
+
+
+Deviceimages_webservice_call()
+{
+  
+let data1 = new URLSearchParams();
+  
+  
+if(this.usertype == "admin")
+{
+  
+data1.append('adminid',this.userid); 
+
+}
+else
+{
+  
+data1.append('technicianid',this.userid);
+ 
+}
+
+
+let body = data1.toString();
+
+console.log(body);
+  this.webservice.getuser(body, this.datashare.Display_Deviceimages_API).subscribe(data =>{
+
+ this.deviceimages_List=data.json().deviceimages;
+ console.log(this.deviceimages_List);
+
 
   err =>
   {
@@ -306,6 +390,24 @@ DeviceimageDialog() {
 
 
 
+AboutDialog() {
+    this.dialog.open(AboutComponent);
+
+
+
+
+  }
+
+
+
+
+
+
+
+
+
+
+
 Edit_Site_Details($event, site)
 {
 
@@ -427,7 +529,7 @@ url:string;
 
 if(this.datashare.edit_client=="Edit")
 {
-  console.log("venkatesh"+this.datashare.edit_client);
+  console.log("edit client details"+this.datashare.edit_client);
 
 this.name=this.datashare.Edit_Client_Detials.name;
 this.phone=this.datashare.Edit_Client_Detials.phone;
@@ -724,7 +826,7 @@ this.poweroff =true;
 
 
 
-console.log("venaktesh"+this.role);
+console.log("Role"+this.role);
 
 
 }
@@ -1119,7 +1221,7 @@ console.log(this.All_testSuties_Names_count);
 for(var i=0;i<this.All_testSuties_Names_count;i++)
 {
   
-console.log("venkatesh"+i);
+
 
 this.All_testSuties_Names=this.All_testSuties_Names_data[i];
 
@@ -1679,7 +1781,7 @@ this.AT_Simcard=true;
 this.AT_Activation_Lock=true;
 this.AT_Carrier_Lock=true;
 this.AT_Warranty=true;
-  console.log("venkatesh");
+  
 }
 
 
@@ -1703,7 +1805,7 @@ this.AT_Simcard=false;
 this.AT_Activation_Lock=false;
 this.AT_Carrier_Lock=false;
 this.AT_Warranty=false;
-  console.log("venkatesh");
+ 
 }
 
 
@@ -2266,7 +2368,7 @@ device:boolean = true;
 
 if(this.datashare.edit_site=="Edit")
 {
-  console.log("venkatesh"+this.datashare.edit_client);
+  console.log("edit client details"+this.datashare.edit_client);
 
 this.name=this.datashare.Edit_Site_Detials.name;
 this.phone=this.datashare.Edit_Site_Detials.phone;
@@ -2400,6 +2502,58 @@ alert("error getting")
 
 
 
+
+@Component({
+  selector: 'about-component',
+  templateUrl: './app/Settings/about/about.component.html',
+    styleUrls: ['./app/Settings/about/about.component.css']
+})
+
+
+export class AboutComponent {
+
+
+opensourcelicense:Array<any>;
+
+
+  constructor(public dialogRef: MdDialogRef<AboutComponent>,public dialog: MdDialog) {
+
+this.opensourcelicense=[{name:'Angular'},{name:'ngMaterial'},{name:'Material Icons'},{name:'Electron'},{name:'Angular'},{name:'ngMaterial'},{name:'Material Icons'},{name:'Electron'},{name:'Angular'},{name:'ngMaterial'},{name:'Material Icons'},{name:'Electron'}];
+
+
+
+  }
+
+LicenseDialog() {
+
+
+    this.dialog.open(LicenseComponent);
+
+  }
+
+
+}
+
+
+
+
+@Component({
+  selector: 'license-component',
+  templateUrl: './app/Settings/about/license/license.component.html',
+    styleUrls: ['./app/Settings/about/license/license.component.css']
+})
+
+
+export class LicenseComponent {
+
+
+
+
+
+  constructor(public dialogRef: MdDialogRef<LicenseComponent>) {
+
+  }
+}
 
 
 
